edd27c7543d28f59d5e991524094e9ca857befd7
grantsagovac1 <grantsagovac1@gmail.com>
2023-01-28 18:38:30 -0500

    commit one

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="App">
+      <header>
+        <Link to="/">Amazona</Link>
+      </header>
+      <main>
+        <Routes>
+          <Route path ="/product/:slug" element={<ProductScreen />} />
+          <Route path="/" element={<HomeScreen />} />
+        </Routes>
+        
+      </main>
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend/src/data.js
@@ -0,0 +1,71 @@
+const data = {
+    products: [
+        // Product 1
+        {
+            name: 'Oak Tree',
+            slug: '3g-oak-tree',
+            category: 'hardwood',
+            image: '/images/product-1.jpg',
+            price: 25,
+            countInStock: 15,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 14,
+            description: '3 gallon oak tree in a pot',
+        },
+        // product 2
+        {
+            name: 'Bald Cypress Tree',
+            slug: '3G-Bald-Cypress-Tree',
+            category: 'hardwood',
+            image: '/images/bald-cypress2.jpg',
+            price: 25,
+            countInStock: 15,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 14,
+            description: '3 gallon oak tree in a pot',
+        },
+        // product 3
+        {
+            name: 'Maple Tree',
+            slug: '3G-maple-tree',
+            category: 'hardwood',
+            image: '/images/Big-Leaf-Maple-Med-Etsy-Small_2250x.jpg',
+            price: 25,
+            countInStock: 15,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 14,
+            description: '3 gallon oak tree in a pot',
+        },
+        // product 4
+        {
+            name: 'Pine Tree',
+            slug: '3g-pine-tree',
+            category: 'softwood',
+            image: '/images/Slash_Pine_5G_01.jpg',
+            price: 25,
+            countInStock: 15,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 14,
+            description: '3 gallon oak tree in a pot',
+        },
+        // product 5
+        {
+            name: 'Pine Tree',
+            slug: '5g-pine-tree',
+            category: 'softwood',
+            image: '/images/Slash_Pine_5G_01.jpg',
+            price: 25,
+            countInStock: 15,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 14,
+            description: '3 gallon oak tree in a pot',
+        },
+    ],
+};
+
+export default data;
